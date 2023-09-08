8f51d8f6cc72113b42384b0b9cf10c9f989c5f36
jhonycastellanos <jhonycastellanos12@gmail.com>
2023-09-08 17:10:40 -0500

    New commit

---
+++ src/app/page.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function page() {
+  return (
+    <div className="border border-cyan-400 m-20">
+      <h1 className="text-2xl flex justify-center p-10">
+        Next CI/CD con Github actions
+      </h1>
+    </div>
+  );
+}
+
+export default page;
